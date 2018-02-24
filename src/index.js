15935b14575b1af1634c667cda5c17e62291a314
Michael Anderson <michanders@msn.com>
2018-02-24 17:55:11 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 const awesome = React.createElement('h1', {}, 'An Awesome Person');
 const learn = React.createElement('p', {}, 'Who is learning React');
+const js = React.createElement('li', {}, "JavaScript");
+const js = React.createElement('li', {}, "React");
+const js = React.createElement('li', {}, "Movies");
+const js = React.createElement('li', {}, "IceCream");
+
+
 const list = React.createElement('ul', {className: 'my-interests'}, []);
 
 
