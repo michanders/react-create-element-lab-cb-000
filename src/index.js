335ec22bec9e42fc3cf64fda3053f0a546f9010b
Michael Anderson <michanders@msn.com>
2018-02-24 17:49:06 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
+const meInReact = React.createElement('div', {}, []);
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
